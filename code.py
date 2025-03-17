f99f90ae4f45bd029a9ea1203486e0ad947cb85e
brodiecWHS <crub23087@wainuiomatahigh.school.nz>
2025-03-17 14:27:39 +1300

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,7 @@
+import time
 print("Hello welcome to my quiz")
 print("Please answer all questions truthfully and to your best ability")
+print("Please be case sensitive")
 print("Just a few questions to get to know you")
 print("What is your name? ")
 str_userName = input()
@@ -18,14 +20,33 @@ while True:
 print("And finally whats something you enjoy doing?")
 user_fun = input()
 print ("So your name is {}, You are {} Years old and enjoy {}".format(str_userName,int_userAge,user_fun))
+time.sleep(1)
 print("Thanks for answering those questions")
 print ("Now we can get onto the real questions!")
-print("Question 1")
+while True: 
+    print("Question 1")
+    d1 = input("Do you want: A) Jam. B) Honey [A/B]: ").lower() 
 
-#how to add multi choice questions
-print("do you want jam or honey")
-d1 = input("Do you want : A) Jam. B)Honey [A/B]: ")
-if d1 == "A":
-    print("Yum, Jam")
-if d1 == "B":
-    print("Sticky, Honey")
+    if d1 == "a":
+        print("Yum, Jam")
+        break  
+    elif d1 == "b":
+        print("Sticky, Honey")
+        break  
+    else:
+        print("Please enter A or B.")
+time.sleep(1)
+while True:
+    print("Question 2")
+    time.sleep(1)
+    d2 = input("Do you want: A) Jam. B) Honey [A/B]: ").lower() 
+
+    if d2 == "a":
+        print("Yum, Jam")
+        break  
+    elif d2 == "b":
+        print("Sticky, Honey")
+        break  
+    else:
+        print("Please enter A or B.")
+        continue
