258625da69d24f750b02f2761144998391bc051a
brodiecWHS <crub23087@wainuiomatahigh.school.nz>
2025-04-29 12:37:39 +1200

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,30 @@
 import time
-
+""""Imported time to use the time sleep function"""
+   
+questions = [
+        ("1. Who is the main character in Hunt for the Wilderpeople? "
+         "A) Ricky Baker B) Billy Tait C) Tim Johnson D) Johnny Blake", 'a'),
+        ("2. What is the name of Ricky's foster uncle? "
+         "A) Ricky Baker B) Billy Tait C) Tim Johnson D) Johnny Blake", 'b'),
+        ("3. Which animal do Hec and Ricky hunt together? "
+         "A) Wild Boar B) Kangaroo C) Deer D) Bear", 'a'),
+        ("4. What does Bella give Ricky for his birthday? "
+         "A) A handmade birthday song and a hot water bottle B) A hunting knife and boots "
+         "C) A video game console D) A pet parrot", 'a'),
+        ("5. What does Paula repeatedly say about Ricky? "
+         "A) 'No child left behind.' B) 'We will find him!' "
+         "C) 'He's a criminal now.' D) 'I never lose a case.'", 'a'),
+        ("6. What is the name of Ricky's dog? "
+         "A) Tupac B) Notorious C) Biggie D) Scarface", 'a'),
+        ("7. Which book is Hunt for the Wilderpeople based on? "
+         "A) Wild Pork and Watercress B) The Bushman's Guide C) Into the Wild D) Runaway Boy", 'a'),
+        ("8. What does Hec reveal about himself while on the run with Ricky? "
+         "A) He is illiterate and never learned to read B) He used to be a famous rugby player "
+         "C) He has been in witness protection D) He once lived in Australia", 'a'),
+        ("9. Did Hec make the cake for Ricky's birthday? A) Yes B) No", 'b'),
+        ("10. What does Ricky call Hec? A) Uncle B) Hec C) Dad D) Friend", 'a')
+    ]
+"""Put the questions at the top as it is a variable"""
 
 def ask_question(question, correct_answer, options):
     """Prompts user with a question and validates input."""
@@ -39,30 +64,6 @@ def run_quiz():
     time.sleep(1)
     print("Thanks for answering those questions.")
     print("Now we can get onto the real questions!")
-
-    questions = [
-        ("1. Who is the main character in Hunt for the Wilderpeople? "
-         "A) Ricky Baker B) Billy Tait C) Tim Johnson D) Johnny Blake", 'a'),
-        ("2. What is the name of Ricky's foster uncle? "
-         "A) Ricky Baker B) Billy Tait C) Tim Johnson D) Johnny Blake", 'b'),
-        ("3. Which animal do Hec and Ricky hunt together? "
-         "A) Wild Boar B) Kangaroo C) Deer D) Bear", 'a'),
-        ("4. What does Bella give Ricky for his birthday? "
-         "A) A handmade birthday song and a hot water bottle B) A hunting knife and boots "
-         "C) A video game console D) A pet parrot", 'a'),
-        ("5. What does Paula repeatedly say about Ricky? "
-         "A) 'No child left behind.' B) 'We will find him!' "
-         "C) 'He's a criminal now.' D) 'I never lose a case.'", 'a'),
-        ("6. What is the name of Ricky's dog? "
-         "A) Tupac B) Notorious C) Biggie D) Scarface", 'a'),
-        ("7. Which book is Hunt for the Wilderpeople based on? "
-         "A) Wild Pork and Watercress B) The Bushman's Guide C) Into the Wild D) Runaway Boy", 'a'),
-        ("8. What does Hec reveal about himself while on the run with Ricky? "
-         "A) He is illiterate and never learned to read B) He used to be a famous rugby player "
-         "C) He has been in witness protection D) He once lived in Australia", 'a'),
-        ("9. Did Hec make the cake for Ricky's birthday? A) Yes B) No", 'b'),
-        ("10. What does Ricky call Hec? A) Uncle B) Hec C) Dad D) Friend", 'a')
-    ]
 
     for question, correct_answer in questions:
         if ask_question(question, correct_answer, 'A/B/C/D'):
